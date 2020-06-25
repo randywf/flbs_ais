62428de6cb0c4ad02a921a1a0299a95b43bbba7a
Randy Flores <5883513+randywf@users.noreply.github.com>
2020-06-25 14:11:04 -0600

    Updated nas_csv script
    Switched from csv module to pandas
    Created a method for use from other files

--- nas_csv.py
+++ nas_csv.py
@@ -6,37 +6,29 @@ csv file to <original filename>_modified.csv
 Usage: nas_csv.py <csv_file> <"comma","separated","columns","to","keep">
 """
 
-import csv
 import sys
+import pandas as pd
+import numpy as np
 
-# NOTE: Use pandas instead?
-#from pandas import read_csv
 
-# TODO: Add method for using this module in other files
+def process_csv_df(csv_file, keep_columns):
+    df = pd.read_csv(csv_file, low_memory=False)
+    column_names = df.columns
+    drop_columns = np.setdiff1d(column_names, keep_columns)
+
+    df = df.drop(drop_columns, axis=1)
+    df = df.rename(columns={'Specimen Number':'key',
+                            'Latitude':'latitude',
+                            'Longitude':'longitude'})
+    df = df.set_index('key')
+    df = df.dropna()
+    
+    return df
+
 
 if __name__ == "__main__":
-    args = sys.argv
     filename = sys.argv[1][:sys.argv[1].rfind('.')]
-    request_columns = sys.argv[2].split(',')
-    request_columns_num = []
-    # TODO: Add error checking for input
-
-    with open(args[1]) as csv_in:
-        # NOTE: Would dictreader/writer be a better choice?
-        csv_reader = csv.reader(csv_in, delimiter = ',')
-        column_names = csv_reader.__next__()
-
-        column_number = 0
-        for column in column_names:
-            for request_column in request_columns:
-                if request_column == column:
-                    request_columns_num.append(column_number)
-            column_number += 1
-        
-        with open(f"{filename}_modified.csv", 'w') as csv_out:
-            csv_writer = csv.writer(csv_out, delimiter = ',')
-            for row in csv_reader:
-                new_row = []
-                for num in request_columns_num:
-                    new_row.append(row[num])
-                csv_writer.writerow(new_row)
+    new_filename = f"{filename}_modified.csv"
+    keep_columns = sys.argv[2].split(',')
+    test_df = process_csv_df(sys.argv[1], keep_columns)
+    test_df.to_csv(new_filename)
